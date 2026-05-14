139bfdbc0f7693773b7558e6c964b235a19c5dc4
Priyanshu Kumar <priyanshupathak804@gmail.com>
2026-05-14 15:48:27 +0530

    Added root route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+   res.send("Backend is running successfully");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
